0f32651ebd80a7b2c2085d7f18636108844b29cb
Aituglo <contact@aituglo.com>
2020-04-08 22:39:41 +0200

    Change install

--- install_debian_script.sh
+++ install_debian_script.sh
@@ -11,7 +11,7 @@ echo "Onyx Install"
 echo "Dependencies Install"
 echo "***"
 apt-get -y update
-apt-get -y install unzip git build-essential cmake python3 screen python3-babel python3-software-properties python3-pip python3-setuptools python3-dev python3-virtualenv openssl libssl-dev memcached python3-memcache libmemcached-dev zlib1g-dev libffi-dev swig3.0 swig sox python3-pyaudio libatlas-base-dev libportaudio2 libportaudiocpp0 portaudio19-dev flac mplayer mpg321
+apt-get -y install unzip curl git build-essential cmake python3 screen python3-babel python3-software-properties python3-pip python3-setuptools python3-dev python3-virtualenv openssl libssl-dev memcached python3-memcache libmemcached-dev zlib1g-dev libffi-dev swig3.0 swig sox python3-pyaudio libatlas-base-dev libportaudio2 libportaudiocpp0 portaudio19-dev flac mplayer mpg321
 
 
 echo "Fann Install"
@@ -26,7 +26,7 @@ pip3 install virtualenv
 apt-get -y install nodejs npm
 npm install -g pm2
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
-sudo apt update && sudo apt install yarn
+apt update && apt install yarn
